812235e2d321fda6d537b50ad137c9e279cc4240
Ricky Roberts <roberts7411@gmail.com>
2019-09-20 13:48:35 -0400

    Activity 4 Completed

---
+++ src/StringExplorer.java
@@ -0,0 +1,44 @@
+/**
+ * A program to allow students to try out different 
+ * String methods. 
+ * @author Laurie White
+ * @version April 2012
+ */
+public class StringExplorer
+{
+	public static void main(String[] args)
+	{
+		String sample = "The quick brown fox jumped over the lazy dog.";
+		
+		//  Demonstrate the indexOf method.
+		int position = sample.indexOf("quick");
+		System.out.println ("sample.indexOf(\"quick\") = " + position);
+		
+		//  Demonstrate the toLowerCase method.
+		String lowerCase = sample.toLowerCase();
+		System.out.println ("sample.toLowerCase() = " + lowerCase);
+		System.out.println ("After toLowerCase(), sample = " + sample);
+				
+		//  Try other methods here:
+                
+                // Demonstrate toUpperCase method
+                String upperCase = sample.toUpperCase();
+                System.out.println ("sample.toUpperCase() = " + upperCase);
+		System.out.println ("After toUpperCase(), sample = " + sample);
+                
+                // Demonstrate length METHOD
+                int l = sample.length();
+                System.out.println ("sample.length() = " + l);
+                
+               // Why is the method emphasised
+               String[] students = {"charlie", "liam", "colt"};
+               int arrayLength = students.length;
+               System.out.println("students.length = " + arrayLength);
+               int studentOneLength = students[1].length();
+               
+               // substrings
+               String sub1 = sample.substring(5);
+               
+	}
+}
+
